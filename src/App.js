b8860641c10b6eb524d1e621eb38b74b3c66f3bb
Saurabh Singh <dashingsaurabh1996@gmail.com>
2025-06-05 02:03:32 +0530

    added App.js to src

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div className="App">
+      <h1 className='bg-red-300'>Welcome to the App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { App } from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App />
   </React.StrictMode>
 );
